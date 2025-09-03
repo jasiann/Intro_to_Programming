1448f97ef5e0bbfe38cbcdf5cba41ece7e5c2a2e
jasiann <109830872+jasiann@users.noreply.github.com>
2025-09-03 12:17:42 -0400

    Add files via upload

---
+++ Week2Class2-Jason_cao.py
@@ -0,0 +1,61 @@
+age = int(input("what is your age\n"))
+decimal = float(input("what is your "))
+name = input()
+
+print( age, decimal, name)
+
+#------------------------------------------------------------------------------------------
+
+## Veriables: 
+
+x = 5 
+y = x + 1
+print(y)
+x = 9
+y = x + 3 
+print (y)
+#------------------------------------------------------------------------------------------
+
+shirt_price = 40 
+pants_price = 30 
+shirt_price = pants_price
+pants_price = pants_price * 3 
+print(pants_price)
+totalPrice = pants_price + shirt_price
+print(totalPrice)
+
+
+#formatting
+
+print(f'total price is,{totalPrice:.3f}')
+print(f'total price is, {totalPrice:.2f}')
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#type and id of variables: 
+
+print(type(5))
+print(type(2.3))
+print(type("jason"))
+
+print(id(1))
+print(id(1.3))
+print(id("jason"))
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Arithemtic Functions: 
+
+x = 1 
+y = 2 
+z = 0
+
+addition = Z = y 
+subtraction = y - x 
+multiplication = y * y 
+division = x / y 
+exponets = y ** y
+
+print(addition, subtraction, multiplication, division, exponets)
+
+print(f'{addition:.2f}')
+
