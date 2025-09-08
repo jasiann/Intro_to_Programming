acc0b71463af545c636dafac18472178d0cd8caf
jasiann <109830872+jasiann@users.noreply.github.com>
2025-09-08 12:23:40 -0400

    Add files via upload

---
+++ Week3Class1Cao_Jason.py
@@ -0,0 +1,19 @@
+# Jason Cao
+print('Welcome to python lab!')
+print("week-3 project")
+
+width1 = int(input("What is the width of your first rectangle?\n")) # user input for width 1 for rectangle 1
+length1 = int(input("What is the length of your first rectangle?\n")) # user input for length 1 for rectangle 1
+
+area_rect1 = width1 * length1 # variable to make the code more organized 
+
+print("Area of the first rectangle is:", area_rect1, "sq cm")# printing the area for rectangle 1 
+
+width2 = int(input("What is the width of your second rectangle?\n")) # user input for width 1 for rectangle 1
+length2 = int(input("What is the length of your second rectangle?\n")) # user input for length 2 for rectangle 2
+
+area_rect2 = width2 * length2 # variable to make the code more organized
+
+print("Area of the second rectangle is:", area_rect2, "sq cm") # printing the area for rectangle 1 
+print("Enjoy calculating area")
+
