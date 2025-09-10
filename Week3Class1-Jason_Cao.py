8612d140df895fa2c8e11317dd91096a5ce04e42
jasiann <109830872+jasiann@users.noreply.github.com>
2025-09-10 12:07:25 -0400

    Add files via upload

---
+++ Week3Class1-Jason_Cao.py
@@ -0,0 +1,64 @@
+# math pre-define function 
+
+import math 
+
+number = 49 
+
+print(int(math.sqrt(number)))
+
+#----------------------------------------
+
+#math pre-defined function 
+#ceil()
+
+x = 3.4
+y = -10
+
+print("rounding up:", math.ceil(x))
+
+print("rounding down:", math.floor(x))
+
+print(int("absolute value",math.fabs(y)))
+
+#-----------------------------------------
+
+#Division Rules:
+
+a = 3 
+
+print( a / 2 )
+
+print( a // 2 )
+
+#-----------------------------------------
+
+#modlo --> using % to find remender 
+
+
+y = 27 
+
+print(y % 4) 
+
+#-----------------------------------------
+
+#string Variables
+
+name = 'lily'
+book_name = "The Lord of the Rings"
+
+print(len(book_name))
+
+# The Len() 
+
+print(len(book_name))
+
+print(book_name[4])
+print(book_name[20])
+print(book_name[21]) # this will result in a indexing error
+
+number = [1,2,10.5,'lily']
+
+print(number[2])
+print(number[3])
+
+
